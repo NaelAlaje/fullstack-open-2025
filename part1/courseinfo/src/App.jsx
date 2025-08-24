1a883aed21ecce9eea43418efb4b531b4aeb4d1b
naelalaji <na2el_alaji@hotmail>
2025-08-24 20:48:07 +0200

    Finish exercise 1.1 courseinfo step 1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,37 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part, index) => (
+      <p key={index}>
+        {part.name} {part.exercises}
+      </p>
+    ))}
+  </>
+);
+
+const Total = ({ parts }) => {
+  let total = 0;
+  for (const part of parts) {
+    total += part.exercises;
+  }
+  return <p>Number of exercises {total}</p>;
+};
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
